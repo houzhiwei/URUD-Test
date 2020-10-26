4e47a6387d931afc81515e707bbe1458f27fc2c1
Zack Hou <chihwei.hou@gmail.com>
2020-10-26 11:19:10 +0800

    Create model

---
+++ Pazzo CRUD/Pazzo CRUD/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pazzo_CRUD.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public int DeptID { get; set; }
+
+    }
+}
