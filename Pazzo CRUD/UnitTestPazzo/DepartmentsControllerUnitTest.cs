185e15218a552b16749acb7526cb306e62940df2
Zack Hou <chihwei.hou@gmail.com>
2020-10-27 13:05:39 +0800

    Create DepartmentUnitTest

---
+++ Pazzo CRUD/UnitTestPazzo/DepartmentsControllerUnitTest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pazzo_CRUD.Controllers;
+using Pazzo_CRUD.Models;
+using UnitTestPazzo.Repositories;
+
+namespace UnitTestPazzo
+{
+    [TestClass]
+    public class DepartmentsControllerUnitTest
+    {
+        Department dept1;
+        List<Department> depts = null;
+        DummyDepartmentsRepository deptsRepo = null;
+        UnitOfWork uow = null;
+        DepartmentsController controller = null;
+
+        public DepartmentsControllerUnitTest()
+        {
+            dept1 = new Department { Id=1, name = "業務一部", createDate = DateTime.Now };
+            depts = new List<Department>
+            {
+                dept1,
+                new Department {Id=2, name = "業務部", createDate = DateTime.Now},
+                new Department {Id=3, name = "行政部", createDate = DateTime.Now},
+                new Department {Id=4, name = "生產部", createDate = DateTime.Now},
+                new Department {Id=5, name = "工程部", createDate = DateTime.Now},
+
+            };
+
+            // 建立假的Repository
+            deptsRepo = new DummyDepartmentsRepository(depts);
+
+            // 建立測試用的UnitOfWork
+            uow = new UnitOfWork(deptsRepo);
+
+            // 建立Department 控制項 用來測試與傳送uow 參數
+            controller = new DepartmentsController(uow);
+        }
+
+
+        [TestMethod]
+        public void Create()
+        {
+            // 建立假資料
+            Department newDept = new Department { Id = 6, name = "總經理室", createDate = DateTime.Now };
+
+            // 執行
+            controller.Create(newDept);
+
+            // 取得所有資料
+            List<Department> depts = deptsRepo.GetAllDepartments();
+
+            CollectionAssert.Contains(depts, newDept);
+        }
+
+        [TestMethod]
+        public void Edit()
+        {
+            // 建立筆要驗證修改的資料
+            Department eDept = new Department { Id = 2, name = "業務部"};
+
+            // 執行修改
+            controller.Edit(eDept);
+
+            // 回傳所有資料
+            List<Department> depts = deptsRepo.GetAllDepartments();
+
+            CollectionAssert.Contains(depts, eDept);
+        }
+
+        [TestMethod]
+        public void Delete()
+        {
+            // 呼叫確認刪除的方法
+            controller.DeleteConfirmed(1);
+
+            // 取得目前所有的資料
+            List<Department> depts = deptsRepo.GetAllDepartments();
+
+            CollectionAssert.DoesNotContain(depts, dept1);
+        }
+    }
+}
---
+++ Pazzo CRUD/UnitTestPazzo/Repositories/DummyDepartmentsRepository.cs
@@ -0,0 +1,50 @@
+﻿using Pazzo_CRUD.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTestPazzo.Repositories
+{
+    class DummyDepartmentsRepository: IDepartment
+    {
+        List<Department> m_depts = null;
+
+        public DummyDepartmentsRepository(List<Department> depts)
+        {
+            this.m_depts = depts;
+        }
+
+        public void AddDepartment(Department dept)
+        {
+            m_depts.Add(dept);
+        }
+
+        public void DeleteDepartment(Department dept)
+        {
+            m_depts.Remove(dept);
+        }
+
+        public List<Department> GetAllDepartments()
+        {
+            return m_depts;
+        }
+
+        public Department GetDepartmentById(int? id)
+        {
+            return m_depts.SingleOrDefault(dept => dept.Id == id);
+        }
+
+        public void Save()
+        {
+            // Nothing to do here
+        }
+
+        public void UpdateDepartment(Department dept)
+        {
+            int id = dept.Id;
+            Department deptToUpdate = m_depts.SingleOrDefault(b => b.Id == id);
+            DeleteDepartment(deptToUpdate);
+            m_depts.Add(dept);
+        }
+
+    }
+}
